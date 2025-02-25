5d7ad4a2e87e76f8d887983adde210a8cbf68602
Abhi5738 <Abhimanyukumawat7405@gmail.com>
2025-02-25 22:47:13 +0530

    code 1o

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-"use client"
+"use client";
 import "./globals.scss";
 import { Footer, Navbar } from "./components";
 import { AuthProvider } from "./providers";
-
-
 
 export default function RootLayout({
   children,
@@ -12,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`main_bg h-full text-white w-full`}>
+      <body className={`main_bg h-full p-4 text-white w-full`}>
         <AuthProvider>
           <div>
             <Navbar />
